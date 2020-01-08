d09524dd19f887f2120244537c62d022420f5312
Ron Martin <ronmartin89@gmail.com>
2020-01-08 14:55:21 -0800

    renamed app.js to index.js per the challenge requirements.

---
+++ index.js
@@ -0,0 +1,8 @@
+// Create an app that allows users to add, check, uncheck, and remove items
+// from shopping list
+
+// To Complete this challenge:
+// Using DOM manipulation and traversal to dynamically add and remove HTML elements and apply styles.
+// Linking to an externally hosted library (jQuery) and a locally hosted JavaScript file (index.js).
+// Linking to your application JavaScript file from the index.html page.
+// Using this and event delegation
